cd46be3cc5b07d50b98731d4cc1304403ed11411
Pierre Payet <pierre.payet@outlook.com>
2018-12-10 01:24:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,204 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+		<Router>
+		  <div>
+			<ul style={styles.nav}>
+			  <li style={styles.li}>
+				<Link to="/">Events</Link>
+			  </li>
+			  <li style={styles.li}>
+				<Link to="/articles">Articles</Link>
+			  </li>
+			  <li style={styles.li}>
+				<Link to="/authors">Authors</Link>
+			  </li>
+			  <li style={styles.li}>
+				<Link to="/reviewers">Reviewers</Link>
+			  </li>
+			  <li style={styles.li}>
+				<Link to="/login">Login</Link>
+			  </li>
+			</ul>
+
+			<hr />
+
+			<Route exact path="/" component={Events} />
+			<Route path="/articles" component={Articles} />
+			<Route path="/authors" component={Authors} />
+			<Route path="/reviewers" component={Reviewers} />
+			<Route path="/login" component={Login} />
+			<Route path="/register" component={Register} />
+		  </div>
+		</Router>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            React Hello World
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+function Events() {
+  return (
+    <div>
+      <h2>Event</h2>
+    </div>
+  );
+}
+
+function Articles() {
+  return (
+    <div>
+      <h2>Articles</h2>
+    </div>
+  );
+}
+
+function Authors() {
+  return (
+    <div>
+      <h2>Authors</h2>
+    </div>
+  );
+}
+
+function Reviewers() {
+  return (
+    <div>
+      <h2>Reviewers</h2>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+	  <div style={styles.login}>
+		<h3>Sign In</h3>
+		<form>
+			<input type="mail" placeholder="Email Address" value="" name="email" style={styles.loginInput} />
+			<input type="password" placeholder="Password" value="" name="password" style={styles.loginInput} />
+			<input type="submit" value="Confirm" style={styles.loginSubmit} />
+		</form>
+		<a href="#">Forgot password ?</a>
+		<br /><br /><br />
+		
+		<p>Not a member ? <Link to="/register">Register !</Link></p>
+	  </div>
+    </div>
+  );
+}
+
+function Register() {
+	return (
+		<div>
+			<div style={styles.register}>
+				<h3>Register</h3>
+				
+				<form>
+					<p>Email</p>
+					<input type="mail" placeholder="Email address" value="" name="email" style={styles.registerInput} />
+					
+					<p>Password</p>
+					<input type="password" placeholder="********" value="" name="password" style={styles.registerInput} />
+					
+					<p>Confirm Password</p>
+					<input type="password" placeholder="********" value="" name="confirm_password" style={styles.registerInput} />
+					
+					<p>Birthdate</p>
+					<input type="date" value="" name="birthdate" />
+					
+					<p><input type="checkbox" value="" name="terms_box" />I've read and accept <a href="#">The Terms of Service</a></p>
+					
+					<input type="submit" value="Confirm" style={styles.registerSubmit} />
+				</form>
+			</div>
+		</div>
+	);
+}
+
+const styles = {};
+
+styles.nav = {
+  padding: 0,
+  margin: 0,
+  marginLeft: "350px",
+  marginTop: "20px",
+  height: "50px",
+  width: "100px",
+  display: "flex"
+};
+
+styles.li = {
+  marginRight: "150px",
+  listStyleType: "none"
+};
+
+styles.login = {
+	width: "400px",
+	height: "500px",
+	marginLeft: "550px",
+	marginBottom: "100px",
+	backgroundColor: "#D0D0D0",
+	border: "1px solid black"
+};
+
+styles.loginInput = {
+	width: "300px",
+	height: "30px",
+	marginTop: "20px"
+};
+
+styles.loginSubmit = {
+	marginTop: "20px",
+	width: "250px",
+	height: "30px",
+	backgroundColor: "#bae7ff"
+};
+
+styles.register = {
+	width: "400px",
+	height: "500px",
+	marginLeft: "550px",
+	marginBottom: "100px",
+	backgroundColor: "#D0D0D0",
+	border: "1px solid black"
+};
+
+styles.registerInput = {
+	width: "250px",
+	height: "30px"
+};
+
+styles.registerSubmit = {
+	marginTop: "20px",
+	width: "250px",
+	height: "30px",
+	backgroundColor: "#bae7ff"
+};
+
+export default App;
