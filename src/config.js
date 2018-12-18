aba0c569826b4003f20884f73cf9c91e39d9f968
orzechdev <tarasiukdawid@gmail.com>
2018-12-18 22:30:05 +0100

    Add optional IP address to api in config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080',//Or http://159.89.7.101:8080/api
   clientUrl: 'http://localhost:3000',
   colorAppAccent: '#1890FF',
 };
