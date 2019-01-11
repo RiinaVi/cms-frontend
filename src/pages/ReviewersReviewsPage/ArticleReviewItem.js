3dca0acf7d5533a5787ed312000c6c7db66aefda
Pierre Payet <pierre.payet@outlook.com>
2019-01-11 12:54:10 +0100

    Add files via upload

--- src/pages/ReviewersReviewsPage/ArticleReviewItem.js
+++ src/pages/ReviewersReviewsPage/ArticleReviewItem.js
@@ -4,19 +4,6 @@ import {Icon, Rate} from 'antd';
 import React, {Component} from 'react';
 
 class ArticleReviewItem extends Component {
-    constructor(props) {
-        super(props);
-        this.props.articleReviewData.average = Number(this.props.articleReviewData.average).toFixed(1);
-        if (this.props.articleReviewData.profile_image_url != null) {
-            this.userImage =
-                <div className="profileImage">
-                    <img src={this.props.articleReviewData.profile_image_url} alt=""/>
-                </div>
-        } else {
-            this.userImage = <Icon className="userImage" type="user"/>
-        }
-    }
-
     render() {
         return (
             <div className="ArticleReviewItem">
@@ -25,14 +12,14 @@ class ArticleReviewItem extends Component {
                     <div className="articleReviewerName">{this.props.articleReviewData.name}</div>
                 </div>
                 <div className="averageScore">
-                        <Rate className="rateStars" value={parseFloat(this.props.articleReviewData.average)} allowHalf
+                        <Rate className="rateStars" value={parseFloat(this.props.articleReviewData.grade)} allowHalf
                               defaultValue={0} disabled={true}/>
                         <div className="rateScore">
-                            {this.props.articleReviewData.average}
+                            {this.props.articleReviewData.grade}
                         </div>
                     </div>
 				<div className="articleReviewDate">
-                    <span className="articleReviewDate">{this.props.articleReviewData.publication_date}</span>
+                    <span className="articleReviewDate">{this.props.articleReviewData.comment}</span>
                 </div>
             </div>
         );
