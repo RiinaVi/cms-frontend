2f4519e8cf9e8065a8e504a81b7793a2d111df63
Marta <martmotica@gmail.com>
2019-01-23 09:48:33 +0100

    Add filter in presentation page

--- src/pages/PresentationsPage/index.js
+++ src/pages/PresentationsPage/index.js
@@ -8,36 +8,7 @@ class Presentation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      /* Change this state with data retrieved from backend */
-      presentations: [
-        {
-          key: '1',
-          startTime: '15:00',
-          endTime: '17:00',
-          presentationName: 'Animales en peligro',
-          presenterName: 'Sheldon Cooper',
-          room: 'room 1',
-          description: 'This is the description of the presentation'
-        },
-        {
-          key: '2',
-          startTime: '15:00',
-          endTime: '17:00',
-          presentationName: 'Salvemos a las vacas',
-          presenterName: 'Perico el de los Palotes',
-          room: 'room 2',
-          description: 'This is the description of the presentation'
-        },
-        {
-          key: '3',
-          startTime: '15:00',
-          endTime: '17:00',
-          presentationName: 'Tengo un perro rosa',
-          presenterName: 'Mr. Wonderful',
-          room: 'room 3',
-          description: 'This is the description of the presentation'
-        }
-      ]
+      presentations: []
     };
   }
 
@@ -50,47 +21,54 @@ class Presentation extends Component {
   };
 
   render() {
-    const { Column, ColumnGroup } = Table;
-
-    return (
-      <div style={{ margin: '3%' }}>
-        {/* Change hardcoded session name to data from backend */}
-        <h1>Session name</h1>
-        <Table
-          pagination={false}
-          expandedRowRender={presentation => (
-            <p style={{ margin: 0 }}>{presentation.description}</p>
-          )}
-          dataSource={this.state.presentations}
-        >
-          <ColumnGroup title="Presentations">
-            <Column
-              title="Start Time"
-              width="10%"
-              dataIndex="startTime"
-              key="startTime"
-            />
-            <Column
-              title="End time"
-              width="10%"
-              dataIndex="endTime"
-              key="endTime"
-            />
-            <Column
-              title="Name"
-              dataIndex="presentationName"
-              key="presentationName"
-            />
-            <Column
-              title="Presenter"
-              dataIndex="presenterName"
-              key="presenterName"
-            />
-            <Column title="Room" dataIndex="room" key="room" />
-          </ColumnGroup>
-        </Table>
-      </div>
-    );
+    if (this.state.presentations.length === 0) {
+      return <div />;
+    } else {
+      const dataSource = this.state.presentations.filter(
+        presentation =>
+          presentation.session.sessionID == this.props.match.params.sessionId
+      );
+      const { Column, ColumnGroup } = Table;
+      console.log(dataSource);
+      return (
+        <div style={{ margin: '3%' }}>
+          <h1>Session name</h1>
+          <Table
+            pagination={false}
+            expandedRowRender={presentation => (
+              <p style={{ margin: 0 }}>{presentation.description}</p>
+            )}
+            dataSource={dataSource}
+          >
+            <ColumnGroup title="Presentations">
+              <Column
+                title="Start Time"
+                width="10%"
+                dataIndex="startTime"
+                key="startTime"
+              />
+              <Column
+                title="End time"
+                width="10%"
+                dataIndex="endTime"
+                key="endTime"
+              />
+              <Column
+                title="Name"
+                dataIndex="presentationName"
+                key="presentationName"
+              />
+              <Column
+                title="Presenter"
+                dataIndex="presenterName"
+                key="presenterName"
+              />
+              <Column title="Room" dataIndex="room" key="room" />
+            </ColumnGroup>
+          </Table>
+        </div>
+      );
+    }
   }
 }
 
